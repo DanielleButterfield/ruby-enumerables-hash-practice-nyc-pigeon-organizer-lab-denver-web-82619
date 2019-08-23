35446b7ec163dfeab1053fbf3c694c7b66fa8d0f
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-23 01:04:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.map { |attri, speci|
+    speci.map { |attSpec, birds|
+      birds.map { |names|
+        organized[names] = {:color => [], :gender => [], :lives = > []}
+      }
+    }
+  }
+
+  bird_list = organized.keys
+  data.each do |data_to_transpose, data_spec|
+    data[data_spec].each do |data_lev, assign_bird|
+      assign_bird.each do |bird|
+        bird_list.each do |att|
+          if bird === item
+            organized[att][data_spec] << data_to_transpose.to_s
+          end
+        end
+      end
+    end
+  end
 end
