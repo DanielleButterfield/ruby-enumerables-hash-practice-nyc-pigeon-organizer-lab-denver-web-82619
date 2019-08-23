de4af7d34812864a5d1c9ceee41ce6486277f76d
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-23 01:13:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,24 @@
+def nyc_pigeon_organizer(data)
+  organized = {}
+  data.each do |attri, speci|
+    speci.each do |attSpec, birds|
+      birds.each do |names|
+        organized[names] = {:color => [], :gender => [], :lives = > []}
+      end
+    end
+  end
 
+  bird_list = organized.keys
+  data[:color].each do |data_lev, assign_bird|
+    assign_bird.each do |bird|
+      bird_list.each do |att|
+        if bird === item
+          organized[att][:color] << data_to_transpose.to_s
+        end
+      end
+    end
+  end
+end
 
 def nyc_pigeon_organizer (data)
   organized = {}
@@ -10,16 +30,19 @@ def nyc_pigeon_organizer (data)
       end
     end
   end
-  x = final.keys
-  data[:color].each do |bird_color, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:color] << bird_color.to_s
+
+  bird_list =organized.keys
+
+  data[:color].each do |colors, names|
+    names.each do |comp_bird|
+      bird_list.each do |list_bird|
+        if comp_bird === list_bird
+          organized[comp_bird][:color] << colors.to_s
         end
       end
     end
   end
+
   data[:gender].each do |gender, type|
     type.each do |bird_name|
       x.each do |item|
@@ -29,6 +52,7 @@ def nyc_pigeon_organizer (data)
       end
     end
   end
+
   data[:lives].each do |location, name|
     name.each do |bird_name|
       x.each do |item|
@@ -38,6 +62,5 @@ def nyc_pigeon_organizer (data)
       end
     end
   end
-  puts final
-  return final
+  organized
 end
