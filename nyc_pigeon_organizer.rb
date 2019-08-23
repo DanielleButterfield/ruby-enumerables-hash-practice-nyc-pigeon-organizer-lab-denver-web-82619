5a5bd94979b12ffd5e1fd8b07acdb5827d0e1202
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-23 01:24:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,36 +10,20 @@ def nyc_pigeon_organizer (data)
   end
 
   bird_list = organized.keys
-  bird_atts = data.keys
+  count = 0
 
-  data[:color].each do |colors, names|
-    names.each do |comp_bird|
-      bird_list.each do |list_bird|
-        if comp_bird === list_bird
-          organized[comp_bird][:color] << colors.to_s
+  while data.keys[count]
+    bird_atts = data.keys[count]
+      data[bird_atts].each do |colors, names|
+        names.each do |comp_bird|
+          bird_list.each do |list_bird|
+            if comp_bird === list_bird
+              organized[comp_bird][:color] << colors.to_s
+            end
+          end
         end
       end
+    count += 1
     end
-  end
-
-  data[:gender].each do |genders, names|
-    names.each do |comp_bird|
-      bird_list.each do |list_bird|
-        if comp_bird === list_bird
-          organized[comp_bird][:gender] << genders.to_s
-        end
-      end
-    end
-  end
-
-  data[:lives].each do |home, names|
-    names.each do |comp_bird|
-      bird_list.each do |list_bird|
-        if comp_bird === list_bird
-          organized[comp_bird][:lives] << home
-        end
-      end
-    end
-  end
   organized
 end
