7d8f314f6bdfc0cd6de6040b061b181c6ddf5bd8
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-23 01:06:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,13 +9,11 @@ def nyc_pigeon_organizer(data)
   }
 
   bird_list = organized.keys
-  data.each do |data_to_transpose, data_spec|
-    data[data_spec].each do |data_lev, assign_bird|
-      assign_bird.each do |bird|
-        bird_list.each do |att|
-          if bird === item
-            organized[att][data_spec] << data_to_transpose.to_s
-          end
+  data[:color].each do |data_lev, assign_bird|
+    assign_bird.each do |bird|
+      bird_list.each do |att|
+        if bird === item
+          organized[att][:color] << data_to_transpose.to_s
         end
       end
     end
